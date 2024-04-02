8dc09d89b861d64fc6a6dce63fc99622f7a5af25
Emanuel <emanuel.fernandesdasilva@gmail.com>
2024-04-02 10:27:34 -0300

    initial commit

---
+++ Controllers/AdminTagsContoller.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bloggie.WEB.Controllers
+{
+	public class AdminTagsContoller : Controller
+	{
+		[HttpGet]
+		public IActionResult Add()
+		{
+			return View();
+		}
+	}
+}
---
+++ Models/ViewModels/AddTagRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Bloggie.WEB.Models.ViewModels
+{
+    public class AddTagRequest
+    {
+        public string  Name { get; set; }  
+        
+        public string DisplayName { get; set; }
+    }
+}
